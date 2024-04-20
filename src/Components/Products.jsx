1c5b4c43af09f4288e2db39ae6ebb8883400c04b
flack <163582560+FlackCode@users.noreply.github.com>
2024-04-20 08:45:57 +0200

    responsive products

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,7 +1,17 @@
+import agccover from './Assets/agccover.jpg'
 export default function Products() {
+    const gridItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     return (
-        <div className="h-screen bg-gray-950 p-16">
-
+        <div className=" bg-gray-950 py-16 lg:px-24 xsm: px-8">
+            <div className="grid xsm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 h-full gap-6">
+                {gridItems.map((gridItem) => (
+                    <div className="bg-gray-800 rounded-2xl flex flex-col items-center p-2">
+                        <img src={agccover} className='w-full rounded-2xl' />
+                        <h1 className="text-white font-bold text-xl">Product</h1>
+                        <p className="text-white font-semibold text-base">Short description about the product, info regarding the material, color, etc</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
